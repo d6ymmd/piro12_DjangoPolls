9954ff34eb7fd2b6bd6d2b1aae2814066e1ae1ee
bora shin <bora@boraui-MacBookPro.local>
2020-01-23 02:47:02 +0900

    part5: wirte first django app_automated testing

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,8 +4,8 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
 from django.views import generic
-
 from .models import Question, Choice
+from django.utils import timezone
 
 
 def index(request):
@@ -62,13 +62,24 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+            ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
